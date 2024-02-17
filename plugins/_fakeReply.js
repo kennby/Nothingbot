ade8f13e874f6208c67400d4868852027a4c9086
kennby <153956421+kennby@users.noreply.github.com>
2024-02-17 18:20:53 -0500

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,20 +1,19 @@
-
 import fetch from 'node-fetch'
 export async function before(m,{conn }) {
 	
 	let who = m.sender ? m.sender : conn.user.jid && conn.user.jid ? conn.user.jid : '0@s.whatsapp.net'
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/Ta0uvwJ.jpg')
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/a7ee9c82fe6824c04e8da.jpg')
 	
 	//reply link wa
-   global.rpl = { contextInfo: { externalAdReply: { mediaUrl: wagp, mediaType: 'VIDEO', description: '', title: packname, body: '', thumbnailUrl: pp, sourceUrl: wagp }}} 
+   global.rpl = { contextInfo: { externalAdReply: { mediaUrl: wagp, mediaType: '', description: '', title: packname, body: '', thumbnailUrl: pp, sourceUrl: wagp }}} 
 	
 	//reply link PayPal
-    global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: wapyp, mediaType: 'VIDEO', description: '', title: '', body: '', thumbnailUrl: pp, sourceUrl: wapyp }}}
+    global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: wapyp, mediaType: '', description: '', title: '', body: '', thumbnailUrl: pp, sourceUrl: wapyp }}}
     
     //reply Instagram 
-    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: waig, mediaType: 'VIDEO', description: '', title: '', body: '', thumbnailUrl: pp, sourceUrl: waig }}} 
+    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: waig, mediaType: '', description: '', title: '', body: '', thumbnailUrl: pp, sourceUrl: waig }}} 
 	
 	//reply link yt
-    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: wayt, mediaType: 'VIDEO', description: '' + wayt, title: '', body: '', thumbnailUrl: pp, sourceUrl: wayt }}}
+    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: wayt, mediaType: '', description: '' + wayt, title: '', body: '', thumbnailUrl: pp, sourceUrl: wayt }}}
 
 }
